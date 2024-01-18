f67597148e26878e82b0343051bb382b2c6bc8bb
FRANK OVIEDO <oviedofrank6@gmail.com>
2024-01-18 01:10:21 -0500

    aluraONE_G6001

--- 2034-logica-programacion-1/app.js
+++ 2034-logica-programacion-1/app.js
@@ -1,1 +1,9 @@
-alert("Hola Mundo");
+
+let numeroSecreto = 6;
+let numeroUsuario = prompt("ingresa un numero: ");
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto){
+    alert("acertaste el número")
+}
