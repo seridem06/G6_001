6c9a9795cbb1549789a82b8d0952b83c7384ab8b
FRANK OVIEDO <oviedofrank6@gmail.com>
2024-01-22 22:55:50 -0500

    aluraONE_G6001

--- 2034-logica-programacion-1/app.js
+++ 2034-logica-programacion-1/app.js
@@ -1,20 +1,28 @@
 //variables 
 let numeroSecreto = 2;
-let numeroUsuario = prompt("ingresa un número entre 1 y 10: ");
-
-console.log(numeroUsuario);
-/*este codigo realiza la comparacion*/
-if (numeroUsuario == numeroSecreto){
-    alert(`acertaste el número es: ${numeroUsuario}`);
-}else {
-    if(numeroUsuario > numeroSecreto){
-        alert('el número secreto es menor');
-    }else{
-        alert('el número secreto es mayór')
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraIntento = 'Intento';
+while(numeroUsuario != numeroSecreto) {
+// != es diferente de ...
+    numeroUsuario = prompt("🎇ingresa un número entre 1 y 10🎇: ");
+    console.log(numeroUsuario);
+    /*este codigo realiza la comparacion*/
+    if (numeroUsuario == numeroSecreto){
+        alert(`👌🎉acertaste el número es: ${numeroUsuario} Lo hiciste en ${intentos} ${palabraIntento} 🎉`);
+    }else {
+        if(numeroUsuario > numeroSecreto){
+            alert('🤦‍♂️el número secreto es menor');
+        }else{
+            alert('🤦‍♂️el número secreto es mayor')
+        }
+        //incrementamos en contador cuando no acierta
+        intentos = intentos + 1;
+        palabraIntento = 'Intentos';
+        if (intentos >3){
+            alert(' 😥Llegastes al número máximo de intentos😪');
+            break;
+        }
     }
-    //la condicion no se cumplio
-    //alert("lo siento no acertaste el número");
 }
-
-
 
